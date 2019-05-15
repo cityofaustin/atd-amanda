5cdb7c788e505e629b61e014b67cc898bc3cd26f
John Clary <john.clary@austintexas.gov>
2019-05-15 13:42:13 -0500

    linting w/ black

--- inspector-dash/config.py
+++ inspector-dash/config.py
@@ -4,12 +4,6 @@ SEMGENTS_FILE = "data/segments.csv"
 
 
 CONFIG = {
-    "duration_scoring" : {
-        "source_key" : "TOTAL_DAYS",
-        "score_key" : "duration_score"
-    },
-    "segment_scoring" : {
-        "source_key" : "segment_count",
-        "score_key" : "segment_score"
-    },
+    "duration_scoring": {"source_key": "TOTAL_DAYS", "score_key": "duration_score"},
+    "segment_scoring": {"source_key": "segment_count", "score_key": "segment_score"},
 }
--- inspector-dash/prioritize.py
+++ inspector-dash/prioritize.py
@@ -1,7 +1,7 @@
 # TODO
 # Roadway Classifications
 # DAPCZ Area
-# 
+#
 # DONE
 # # of Permits in Segment
 # Duration of Work Zone
@@ -15,11 +15,12 @@ from config import *
 def total_score(permits, score_keys, total_score_key_name="total_score"):
     return permits
 
+
 def score_permits_by_duration(
-        permits,
-        duration_key=CONFIG["duration_scoring"]["source_key"],
-        score_key=CONFIG["duration_scoring"]["score_key"]
-    ):
+    permits,
+    duration_key=CONFIG["duration_scoring"]["source_key"],
+    score_key=CONFIG["duration_scoring"]["score_key"],
+):
     for permit_id in permits:
 
         try:
@@ -43,12 +44,12 @@ def score_permits_by_duration(
 
 
 def append_key(primary, append_dict, append_key):
-    '''
+    """
     primary: dict that will receive the new key
     append_dict: dict that contains the data that will be appended
       to the primary
     append_key: the key/value that will be added to the primary
-    '''
+    """
 
     for record_id in append_dict:
         # TODO: probably want to handle a KeyError here
@@ -57,8 +58,10 @@ def append_key(primary, append_dict, append_key):
     return primary
 
 
-def score_permits_by_segment_count(permits, count_key="segment_count", score_key=CONFIG["segment_scoring"]["score_key"]):
-    
+def score_permits_by_segment_count(
+    permits, count_key="segment_count", score_key=CONFIG["segment_scoring"]["score_key"]
+):
+
     for permit_id in permits:
         count = permits[permit_id][count_key]
 
@@ -80,7 +83,7 @@ def segments_per_permit(segments):
         permit_id = seg.get("FOLDERRSN")
 
         if permit_id not in segments_count:
-            segments_count[permit_id] = { "segment_count" :  0 }
+            segments_count[permit_id] = {"segment_count": 0}
 
         segments_count[permit_id]["segment_count"] += 1
 
@@ -89,7 +92,7 @@ def segments_per_permit(segments):
 
 def index_by_key(list_of_dicts, key):
     # transform a list of dicts into a dict with a top-level index
-    return { row[key] : row  for row in list_of_dicts}
+    return {row[key]: row for row in list_of_dicts}
 
 
 def main():
@@ -110,19 +113,20 @@ def main():
     # **segment scoring**
     permits_with_seg_count = segments_per_permit(segments)
 
-    permits_weighted_with_seg_count = score_permits_by_segment_count(permits_with_seg_count)
+    permits_weighted_with_seg_count = score_permits_by_segment_count(
+        permits_with_seg_count
+    )
 
     # join segment weight to permits
     permits = append_key(
-        permits,
-        permits_weighted_with_seg_count,
-        CONFIG["segment_scoring"]["score_key"]
+        permits, permits_weighted_with_seg_count, CONFIG["segment_scoring"]["score_key"]
     )
 
     # **duration scoring**
     permits = score_permits_by_duration(permits)
 
     pdb.set_trace()
+
 
 results = main()
 
@@ -132,4 +136,3 @@ results = main()
 
 
 pdb.set_trace()
-

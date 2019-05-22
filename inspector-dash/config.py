1f490a7b28bfcf4922db4f5cb9279efe8c567e57
John Clary <john.clary@austintexas.gov>
2019-05-21 22:56:34 -0500

    linebreak removed

--- inspector-dash/config.py
+++ inspector-dash/config.py
@@ -2,6 +2,7 @@ PERMITS_FILE = "data/permits.csv"
 
 SEMGENTS_FILE = "data/segments.csv"
 
+OUTPUT_FILE = "permits_scored.csv"
 
 CONFIG = {
     "duration_scoring": {"source_key": "TOTAL_DAYS", "score_key": "duration_score"},

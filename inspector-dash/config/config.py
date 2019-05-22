3149a4219c4a1238bbeb6979b3cde2e5952ae5d1
John Clary <john.clary@austintexas.gov>
2019-05-22 08:21:20 -0500

    config reorg

---
+++ inspector-dash/config/config.py
@@ -0,0 +1,10 @@
+PERMITS_FILE = "data/permits.csv"
+
+SEMGENTS_FILE = "data/segments.csv"
+
+OUTPUT_FILE = "permits_scored.csv"
+
+FIELD_CONFIG = {
+    "duration_scoring": {"source_key": "TOTAL_DAYS", "score_key": "duration_score"},
+    "segment_scoring": {"source_key": "segment_count", "score_key": "segment_score"},
+}

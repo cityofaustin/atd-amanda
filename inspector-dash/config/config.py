d4fa7738e471786e3ec3e173345f478b62f5fbdf
John Clary <john.clary@austintexas.gov>
2019-05-22 10:40:29 -0500

    score by road class

--- inspector-dash/config/config.py
+++ inspector-dash/config/config.py
@@ -5,6 +5,14 @@ SEMGENTS_FILE = "data/segments.csv"
 OUTPUT_FILE = "permits_scored.csv"
 
 FIELD_CONFIG = {
-    "duration_scoring": {"source_key": "TOTAL_DAYS", "score_key": "duration_score"},
-    "segment_scoring": {"source_key": "segment_count", "score_key": "segment_score"},
+    "duration": {"source_key": "TOTAL_DAYS", "score_key": "duration_score"},
+    "segments": {"source_key": "segment_count", "score_key": "segment_score", "segment_id_key" : "PROPERTYID"},
+    "road_classes" : {"source_key": "max_road_class", "score_key": "road_class_score"}
+}
+
+GEOM_CONFIG = {
+    "service_id" : "a78db5b7a72640bcbb181dcb88817652",
+    "layer_id" : 0,
+    "primary_key" : "SEGMENT_ID",
+    "outfields" : "ROAD_CLASS, SEGMENT_ID"
 }
--- inspector-dash/prioritize.py
+++ inspector-dash/prioritize.py
@@ -8,39 +8,81 @@
 #
 # IN PROGRESS
 # get_segment_data
-# - do this in chunks, maybe 100 at a time?
+# - do this in chunks, maybe 100 at a time?key
 
 import csv
 import pdb
 
 import agolutil
 
-from config.config import PERMITS_FILE, SEMGENTS_FILE, OUTPUT_FILE, FIELD_CONFIG
+from config.config import (
+    PERMITS_FILE,
+    SEMGENTS_FILE,
+    OUTPUT_FILE,
+    FIELD_CONFIG,
+    GEOM_CONFIG,
+)
+
 from config.secrets import AGOL_CREDENTIALS
 
-pdb.set_trace()
+
+def score_permits_by_road_class(permits, source_key, score_key):
+    #TODO: get road class mapping
+    for permit_id in permits.keys():
+        max_road_class = permits[permit_id].get('max_road_class')
+
+        print("DO SCORE LOGIC HERE")
+        # Critical = 10, Arterial = 7, Collector = 5, Residential = 3
+
+
+
+    return permits
+
+
+
+def get_max_road_class(permits, road_class_segments):
+    for permit_id in permits.keys():
+        segments = permits[permit_id].get('segments')
+        
+        if segments:
+            
+            road_classes = []
+            
+            for segment_id in segments:
+                
+                segment_id = int(segment_id)
+
+                if segment_id in road_class_segments:
+                    road_classes.append(road_class_segments[segment_id]['ROAD_CLASS'])
+
+            if road_classes:
+                permits[permit_id]['max_road_class'] = max(road_classes)
+            
+            else:
+                permits[permit_id]['max_road_class'] = 0
+
+    return permits
+
 
 def get_segment_data(segment_ids, auth):
+    print("get segment data")
+    # ready id string as a sql-like statement
+    where_ids = ", ".join(f"'{x}'" for x in segment_ids)
 
-    where_ids = ", ".join(f"'{x}'" for x in source_ids)
+    where = "{} in ({})".format(GEOM_CONFIG["primary_key"], where_ids)
 
-    if where_ids:
-        where = "{} in ({})".format(cfg["geometry_record_id_field"], where_ids)
+    geometry_layer = agolutil.get_item(
+        auth=auth,
+        service_id=GEOM_CONFIG["service_id"],
+        layer_id=GEOM_CONFIG["layer_id"],
+    )
 
-        geometry_layer = agolutil.get_item(
-            auth=AGOL_CREDENTIALS,
-            service_id=cfg["geometry_service_id"],
-            layer_id=cfg["geometry_layer_id"],
-        )
+    features = geometry_layer.query(
+        where=where, outFields=GEOM_CONFIG["outfields"], returnGeometry=False
+    )
 
-        source_geometries = geometry_layer.query(
-            where=where, outFields=cfg["geometry_record_id_field"]
-        )
+    return [feature.attributes for feature in features]
 
-        if not source_geometries:
-            raise Exception(
-                "No features returned from source geometry layer query"
-            )
 
 def get_all_score_keys(config):
     score_keys = []
@@ -59,9 +101,11 @@ def total_score(permits, score_keys, total_score_key_name="total_score"):
 
 def score_permits_by_duration(
     permits,
-    duration_key=FIELD_CONFIG["duration_scoring"]["source_key"],
-    score_key=FIELD_CONFIG["duration_scoring"]["score_key"],
+    duration_key=FIELD_CONFIG["duration"]["source_key"],
+    score_key=FIELD_CONFIG["duration"]["score_key"],
 ):
+    print("score by duration")
+
     for permit_id in permits:
 
         try:
@@ -100,9 +144,11 @@ def append_key(primary, append_dict, append_key):
 
 
 def score_permits_by_segment_count(
-    permits, count_key="segment_count", score_key=CONFIG["segment_scoring"]["score_key"]
+    permits,
+    count_key="segment_count",
+    score_key=FIELD_CONFIG["segments"]["score_key"],
 ):
-
+    print("score by segment count")
     for permit_id in permits:
         count = permits[permit_id][count_key]
 
@@ -114,21 +160,23 @@ def score_permits_by_segment_count(
     return permits
 
 
-def segments_per_permit(segments):
+def segments_by_permit(segments):
     # count the number of segments per permit
-    # return a dict of permit ID with count of segments
-    segments_count = {}
+    # return a dict of permit ID with count of segments and list of segment ids
+    permit_segments = {}
 
     for seg in segments:
         # FOLDERRSN is the unique permit identifer
         permit_id = seg.get("FOLDERRSN")
 
-        if permit_id not in segments_count:
-            segments_count[permit_id] = {"segment_count": 0}
+        if permit_id not in permit_segments:
+            permit_segments[permit_id] = { "segment_count": 0 }
+            permit_segments[permit_id]["segments"] = []
 
-        segments_count[permit_id]["segment_count"] += 1
+        permit_segments[permit_id]["segment_count"] += 1
+        permit_segments[permit_id]["segments"].append(seg.get("PROPERTYID"))
 
-    return segments_count
+    return permit_segments
 
 
 def index_by_key(list_of_dicts, key):
@@ -152,42 +200,72 @@ def main():
         segments = [row for row in reader]
 
     # **number of segment scoring**
-    permits_with_seg_count = segments_per_permit(segments)
+    permits_with_segments = segments_by_permit(segments)
 
     permits_weighted_with_seg_count = score_permits_by_segment_count(
-        permits_with_seg_count
+        permits_with_segments
     )
 
     # join segment weight to permits
     permits = append_key(
-        permits, permits_weighted_with_seg_count, CONFIG["segment_scoring"]["score_key"]
+        permits,
+        permits_weighted_with_seg_count,
+        FIELD_CONFIG["segments"]["score_key"],
+    )
+
+    # join segment id list to permits
+    permits = append_key(
+        permits,
+        permits_with_segments,
+        "segments"
     )
 
     # **duration scoring**
     permits = score_permits_by_duration(permits)
 
     # **segment road class scoring**
-    pdb.set_trace()
 
-    # testing only. note that this function works without a token (the second param)
-    segment = agolutil.query_atx_street(3185420, None)
+    segment_ids = [segment[FIELD_CONFIG["segments"]["segment_id_key"]] for segment in segments]
 
-    pdb.set_trace()
+    # remove dupes
+    segment_ids = list(set(segment_ids))
+
+    # query segment data from ArcGIS Online in chunks
+    chunksize = 2500
+
+    segment_road_class = []
+
+    for i in range(0, len(segment_ids), chunksize):
+        chunk_of_data = get_segment_data(
+            segment_ids[i : i + chunksize], AGOL_CREDENTIALS
+        )
+        segment_road_class += chunk_of_data
+
+    segment_road_class = index_by_key(segment_road_class, GEOM_CONFIG["primary_key"])
+
+    permits = get_max_road_class(permits, segment_road_class)
+
+    permits = score_permits_by_road_class(permits, FIELD_CONFIG["road_classes"]["source_key"], FIELD_CONFIG["road_classes"]["score_key"])
+    
+    # **total up the score
+    score_keys = get_all_score_keys(FIELD_CONFIG)
+
+    #todo: total up the score from the score keys
+    
     # **write to csv**
-    score_keys = get_all_score_keys(CONFIG)
+    
 
-    output_data = [ permits[permit_id] for permit_id in permits.keys()]
+    output_data = [permits[permit_id] for permit_id in permits.keys()]
 
     with open(OUTPUT_FILE, "w") as fout:
         # assume fieldnames are consistent across all records,
         # so just take the keys from the first entry as fieldnames
         writer = csv.DictWriter(fout, fieldnames=output_data[0].keys())
-        
+
         writer.writeheader()
 
         for row in output_data:
             writer.writerow(row)
-
 
 
 results = main()
@@ -195,4 +273,3 @@ results = main()
 # sort the dicts by seg vals
 # https://stackoverflow.com/questions/613183/how-do-i-sort-a-dictionary-by-value
 # results = sorted(results.items(), key=lambda kv: kv[1], reverse=True)
-

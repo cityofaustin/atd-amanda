044d7023125b2e0cd131a508c46919864f5a2745
Tracy Linder <49930868+TracyLinder@users.noreply.github.com>
2019-07-18 11:38:13 -0500

    PWD Permit Report
    Provide to Ryan Ward the 1st of each month.

---
+++ plsql-queries/pwd-permit-report.sql
@@ -0,0 +1,28 @@
+--DS
+Select f.folderrsn, f.foldertype as Permit_Type, vs.subdesc as Sub_Type, vw.workdesc as Work_Type, f.expirydate as Expiration, f.folderdescription as Permit_Description,
+p.organizationname as Contractor, p.phone1 as Phone#,p.namefirst ||' '|| p.namelast as Contact
+From folder f, folderpeople fp, validsub vs, validwork vw, people p
+Where f.foldertype = 'DS'
+and f.statuscode = 50030 --Expired
+and f.expirydate >= to_date ('&StartDate')
+and vs.subcode = f.subcode
+and vw.workcode = f.workcode
+and fp.peoplecode = 50065 -- ROW Contractor
+and fp.peoplersn = p.peoplersn
+and f.folderrsn = fp.folderrsn
+order by f.expirydate;
+
+--EX
+Select distinct f.folderrsn, f.foldertype as Permit_Type, vs.subdesc as Sub_Type, f.expirydate as Expiration, f.folderdescription as Permit_Description,
+p.organizationname as Contractor, p.phone1 as Phone#,p.namefirst ||' '|| p.namelast as Contact
+From folder f, folderpeople fp, validsub vs, people p, folderfreeform ff
+Where f.foldertype = 'EX'
+and f.statuscode = 50030 --Expired
+and f.expirydate >= to_date ('&StartDate')
+and vs.subcode = f.subcode
+and fp.peoplecode = 50065 -- ROW Contractor
+and fp.peoplersn = p.peoplersn
+and f.folderrsn = fp.folderrsn
+and ff.folderrsn = f.folderrsn
+and ff.c03 = 'Yes'
+order by f.folderrsn;
